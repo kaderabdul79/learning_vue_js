35ee9f29cb7e54c1daf84f508fb3ff9b65cb45a8
kaderabdul <kaderabdul7980@gmail.com>
2022-05-21 08:45:18 +0600

    created first component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>This is signup page</h1>
+</template>
+
+
+<script>
+export default{
+    name:'SignUp'
+}
+</script>
+
+
+<style scoped>
+h1{
+    background-color: 'red'
+}
+</style>
+
+
