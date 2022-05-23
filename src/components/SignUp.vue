68ad53e62e7073e453907c6681fa41343582b2e0
kaderabdul <kaderabdul7980@gmail.com>
2022-05-23 06:44:15 +0600

    design the signup page

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>This is signup page</h1>
+    <img class="logo" src="../assets/logo.jpg" alt="logo.jpg" />
+    <h1>Registration</h1>
+
+    <div class="register">
+        <input type="text" placeholder="Enter name" />
+        <input type="text" placeholder="Enter email" />
+        <input type="password" placeholder="Enter password" />
+        <button class="btn">Signup</button>
+    
+    </div>
+
+
 </template>
 
 
@@ -12,7 +23,30 @@ export default{
 
 <style scoped>
 h1{
-    background-color: 'red'
+    text-align:center;
+}
+img.logo {
+    width: 100px;
+    display: block;
+    margin: 0 auto;
+    background-color: red;
+}
+.register{
+    width: 250px;
+    margin:0 auto;
+}
+.register input{
+    width:200px;
+    display:block;
+    padding:8px 12px;
+    border:1px solid green;
+    margin-bottom:10px;
+}
+.register .btn{
+    width:224px;
+    padding:14px 0px;
+    background:green;
+    border:none;
 }
 </style>
 
