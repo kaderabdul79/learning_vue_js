395b2164e5df5cf177762e0b476b5ffacb7410bb
kaderabdul <kaderabdul7980@gmail.com>
2022-05-26 06:44:15 +0600

    taking user input and using axios send it json server also saved in localStorage

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,27 +1,56 @@
 <template>
+
+<div class="reg-container">
     <img class="logo" src="../assets/logo.jpg" alt="logo.jpg" />
     <h1>Registration</h1>
 
     <div class="register">
-        <input type="text" placeholder="Enter name" />
-        <input type="text" placeholder="Enter email" />
-        <input type="password" placeholder="Enter password" />
-        <button class="btn">Signup</button>
-    
+        <input type="text" v-model="name" placeholder="Enter name" />
+        <input type="text" v-model="email" placeholder="Enter email" />
+        <input type="password" v-model="password" placeholder="Enter password" />
+        <button class="btn" v-on:click="register">Signup</button>
     </div>
 
+</div>
 
 </template>
 
 
 <script>
+import axios from 'axios'
 export default{
-    name:'SignUp'
+    name:'SignUp',
+    data(){
+        return {
+            name:"",email:"",password:""
+        }
+    },
+    methods:{
+        async register(){
+            let result = await axios.post('http://localhost:8000/user',{
+                name:this.name,
+                email:this.email,
+                password:this.password
+            })
+            
+            if(result.status == 201){
+                alert("register done");
+                localStorage.setItem("user-info",JSON.stringify(result.data))
+            }
+        }
+    }
 }
 </script>
 
 
 <style scoped>
+.reg-container{
+    /* height: 100vh;
+    width: 100vh;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin: auto; */
+}
 h1{
     text-align:center;
 }
@@ -32,18 +61,18 @@ img.logo {
     background-color: red;
 }
 .register{
-    width: 250px;
-    margin:0 auto;
+    width: 300px;
+    margin:auto;
 }
 .register input{
-    width:200px;
+    width:300px;
     display:block;
-    padding:8px 12px;
+    padding:12px 12px;
     border:1px solid green;
     margin-bottom:10px;
 }
 .register .btn{
-    width:224px;
+    width:324px;
     padding:14px 0px;
     background:green;
     border:none;
